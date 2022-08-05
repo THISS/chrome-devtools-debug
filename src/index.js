c616f31eeaee8ae07f7e48ca4817cbc8059212e4
Brenton Holswich <brenton.holswich@gmail.com>
2022-08-05 06:27:20 -0700

    added calculator

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
-import "./styles.css";
+import './styles.css'
+import {
+  sum,
+  divide,
+  multiply,
+  toThePower,
+  takeAway,
+  squareRoot,
+  square,
+} from './calculator'
 
-document.getElementById("app").innerHTML = `
+function wrapInHtml(title, toPrint) {
+  return `<div>${title}: ${toPrint}</div>`
+}
+
+// Write Javascript code!
+const appDiv = document.getElementById('app')
+
+const markup = `<main>
+${wrapInHtml('sum', sum(1, 2, 3, 4, 5))}
+${wrapInHtml('subtract', takeAway(1, 2, 3, 4, 5))}
+${wrapInHtml('divide', divide(1, 2, 3, 4, 5))}
+${wrapInHtml('multiply', multiply(1, 2, 3, 4, 5))}
+${wrapInHtml('square', square(3))}
+${wrapInHtml('squareRoot', squareRoot(9))}
+${wrapInHtml('toThePower', toThePower(9, 3))}
+</main>`
+
+appDiv.innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
+  We use the same configuratio as Parcel to bundle this sandbox, you can find more
+  info about Parcel
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
-`;
+`
